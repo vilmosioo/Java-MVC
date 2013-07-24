e36b301a4ab85e9e97d6c97e75e534a5f1dad617
Vilmos Ioo <cool.villi@gmail.com>
2013-07-24 17:05:26 +0100

    Update Repository.java

--- Utilities/Repository.java
+++ Utilities/Repository.java
@@ -1,16 +1,5 @@
 package Utilities;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import Controllers.MainController;
-import Data.Face;
-import Data.Patch;
-import Data.Point;
-
 /**
  * @author Vilmos Ioo
  */
@@ -22,7 +11,6 @@ public class Repository implements IRepository {
 
 	}
 
-	
 	public void connect() {
 		// TODO Connect to a database
 	}
